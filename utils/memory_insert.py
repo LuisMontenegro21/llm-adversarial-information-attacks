57029f531d147c5f1392d947d84fdffed51467a7
LuisMontenegro21 <83621697+LuisMontenegro21@users.noreply.github.com>
2026-08-24 19:07:57 -0600

    Create files and directories

---
+++ utils/memory_insert.py
@@ -0,0 +1,10 @@
+# -----------------------------------------------
+# Memory insertion file
+# This file helps to insert information onto the 
+# -----------------------------------------------
+import abc
+
+
+class MemInsert():
+    def __init__(self):
+        pass
